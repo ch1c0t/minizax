e5c2482528923252d74f2c0a854c5a1d4295e32c
Anatoly Chernow <chertoly@gmail.com>
2018-06-20 15:48:50 +0300

    /start_session responds with 400 if there is no client token
    https://github.com/ch1c0t/zax-specs/blob/b03fd3d0585a8b5550132ce83fa8b09f45bbcf4c/spec/Alice_initiates_session._spec.rb#L17

--- apps/start_session.rb
+++ apps/start_session.rb
@@ -2,11 +2,15 @@ class StartSession
   include Hobby
 
   post do
-    #client_token = request.body.read 44
-    #p client_token
-    relay_token = Base64.strict_encode64 RbNaCl::Random.random_bytes 32
-    difficulty = 0
+    client_token = request.body.read 44
 
-    "#{relay_token}\r\n#{difficulty}"
+    if client_token
+      relay_token = Base64.strict_encode64 RbNaCl::Random.random_bytes 32
+      difficulty = 0
+
+      "#{relay_token}\r\n#{difficulty}"
+    else
+      response.status = 400
+    end
   end
 end
