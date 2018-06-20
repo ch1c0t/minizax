756da2690afb9c3fc00d098fff91fad36e41c1ee
Anatoly Chernow <chertoly@gmail.com>
2018-06-21 02:27:14 +0300

    /start_session responds with 401 if a client token is invalid

--- apps/start_session.rb
+++ apps/start_session.rb
@@ -2,13 +2,20 @@ class StartSession
   include Hobby
 
   post do
-    client_token = request.body.read 44
+    b64_client_token = request.body.read 44
+    client_token = if b64_client_token&.size == 44
+                     b64_client_token.from_b64
+                   end
 
-    if client_token
-      relay_token = Base64.strict_encode64 RbNaCl::Random.random_bytes 32
-      difficulty = 0
+    if b64_client_token
+      if client_token&.size == 32
+        relay_token = Base64.strict_encode64 RbNaCl::Random.random_bytes 32
+        difficulty = 0
 
-      "#{relay_token}\r\n#{difficulty}"
+        "#{relay_token}\r\n#{difficulty}"
+      else
+        response.status = 401
+      end
     else
       response.status = 400
     end
